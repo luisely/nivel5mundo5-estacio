6227df49ff37cc71031d6f6efda2ba3abfce37b2
Fernando Ely <luisely@gmail.com>
2025-06-09 18:26:18 -0300

    ajustes

--- src/routes/contracts.js
+++ src/routes/contracts.js
@@ -5,10 +5,7 @@ import { validateToken } from '../middleware/auth.js';
 
 const router = express.Router();
 
-// Rota para criar contrato
 router.post('/', validateToken, requireAdmin, createContract);
-
-// Rota para obter contratos
 router.get('/:empresa/:inicio', validateToken, getContracts);
 
 export default router;
